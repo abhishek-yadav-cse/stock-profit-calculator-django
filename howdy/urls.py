1a35a9a6feb9accce3b2aad961098cc6792ea047
Abhishek Yadav <abhishek.yadav.cs@gmail.com>
2018-12-02 12:56:54 -0800

    Update urls.py

--- howdy/urls.py
+++ howdy/urls.py
@@ -4,6 +4,6 @@ from howdy import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
-    # url(r'^$', views.AboutPageView.as_view()), # Add this /about/ route
+    # url(r'^$', views.AboutPageView.as_view()), # Add this /about/ route /
     url(r'^result', views.HomePageView.as_view()),
 ]
